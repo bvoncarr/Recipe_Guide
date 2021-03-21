9352d1edba8eb9e2b2fdb3b425acc91b73ddc55e
Beverly <beverlybuscher@gmail.com>
2021-03-20 19:08:45 -0500

    added images, updated html, js & css

--- recipe.js
+++ recipe.js
@@ -1,3 +1,4 @@
+console.log('js is working')
 
 const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
@@ -15,7 +16,7 @@ searchForm.addEventListener("submit", (e) => {
 });
 
 async function fetchAPI() {
-    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appID}&app_key=${apiKey}&from=0&to=10`;
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appID}&app_key=${apiKey}&from=0&to=3`;
     const response = await fetch(baseURL);
     const data = await response.json();
     generateHTML(data.hits);
@@ -29,7 +30,7 @@ function generateHTML(results){
         <div class="item">
             <img src="${result.recipe.image}" alt=" "/>
             <div class="flex-container">
-                <h1 class="title">${result.recipe.label}</h1>
+                <h4 class="title">${result.recipe.label}</h4>
                 <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
             </div>
             <p class="item-data">Calories: ${result.recipe.calories.toFixed(0)}</p>
@@ -40,7 +41,7 @@ function generateHTML(results){
               }</p>
               <p class="item-data">Health-labels!!: ${result.recipe.healthLabels}</p>
               <p class="item-data">Dish-Type: ${result.recipe.dishType}</p>
-              <p class="item-data">Ingredientsssss: ${result.recipe.ingredientLines}</p>
+              <p class="item-data">Ingredients: ${result.recipe.ingredientLines}</p>
             </div>
         </div>
          `;
@@ -49,151 +50,69 @@ function generateHTML(results){
     searchResultDiv.innerHTML = generatedHTML;
 }
 
-//Beverly's code below
 
-function randomDrink() {
+//beverly's code below:
+
+
+// const ingredientContainer = document.querySelector('#ingredientContainer')
+
+function randomCocktail() {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
   .then(
     function(response) {
       if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
         return;
       }
 
       
       response.json().then(function(data) {
-        // console.log(data);
-        displayRandomDrink(data);
+        console.log(data);
+        displayRandomCocktail(data);
       });
     }
   )
   .catch(function(err) {
-    console.log('Fetch Error :-S', err);
+    console.log(err);
   });
 }
 
-randomDrink();
+randomCocktail();
 
-function displayRandomDrink(drink) {
-    console.log(drink.drinks[0].strDrink);
+var button = document.getElementById("random-drink");
+button.addEventListener("click", function(event) {
+    console.log("clicked");
+    location.reload();    
+})
 
+function displayRandomCocktail(cocktail) {
+    
+    console.log(cocktail.drinks[0]);
+    
     let drinkSection = document.querySelector('#drink-section');
 
-    let drinkName = document.createElement('h4');
-
-    drinkName.innerHTML = drink.drinks[0].strDrink;
+    let drinkName = document.createElement('h5');
+    drinkName.innerHTML = cocktail.drinks[0].strDrink;
 
     drinkSection.appendChild(drinkName);
 
     let img = document.createElement('img');
-    img.src = drink.drinks[0].strDrinkThumb;
+    img.src = cocktail.drinks[0].strDrinkThumb;
 
     drinkSection.appendChild(img);
 
+    let i;
 
-    // for(let i = 1; i < 16; i++)
-    //     console.log(i);
+    for(let i = 1; i < 16; i++) {
+        console.log(i);
+        if(cocktail.drinks[0][`strIngredient${i}`] == null || cocktail.drinks[0][`strMeasure${i}`] == null) {
+            break;
+        } 
 
-        let ingredient = document.createElement("p");
-        ingredient.innerHTML = drink.drinks[0][`strIngredient${1}`] + ":";
-
+        let ingredient = document.createElement('h6');
+        ingredient.innerHTML = cocktail.drinks[0][`strMeasure${i}`] + " " + cocktail.drinks[0][`strIngredient${i}`];
+    
         drinkSection.appendChild(ingredient);
-
-          let measure = document.createElement('p');
-        measure.innerHTML = drink.drinks[0][`strMeasure${1}`];
-
-        drinkSection.appendChild(measure)
-
-        let ingredient2 = document.createElement("p");
-        ingredient2.innerHTML = drink.drinks[0][`strIngredient${2}`] + ":";
-
-        drinkSection.appendChild(ingredient2);
-
-            let measure2 = document.createElement('p');
-        measure2.innerHTML = drink.drinks[0][`strMeasure${2}`];
-
-        drinkSection.appendChild(measure2)
-
-         let ingredient3 = document.createElement("p");
-        ingredient3.innerHTML = drink.drinks[0][`strIngredient${3}`] + ":";
-
-        drinkSection.appendChild(ingredient3);
-
-              let measure3 = document.createElement('p');
-        measure3.innerHTML = drink.drinks[0][`strMeasure${3}`];
-
-        drinkSection.appendChild(measure3)
-
-         let ingredient4 = document.createElement("p");
-        ingredient4.innerHTML = drink.drinks[0][`strIngredient${4}`] + ":";
-
-        drinkSection.appendChild(ingredient4);
-
-          let measure4 = document.createElement('p');
-        measure4.innerHTML = drink.drinks[0][`strMeasure${4}`];
-        drinkSection.appendChild(measure4)
-
-         let ingredient5 = document.createElement("p");
-        ingredient5.innerHTML = drink.drinks[0][`strIngredient${5}`] + ":";
-
-        drinkSection.appendChild(ingredient5);
-
-           let measure5 = document.createElement('p');
-        measure5.innerHTML = drink.drinks[0][`strMeasure${5}`];
-        drinkSection.appendChild(measure5)
-
-        let ingredient6 = document.createElement("p");
-        ingredient6.innerHTML = drink.drinks[0][`strIngredient${6}`] + ":";
-
-        drinkSection.appendChild(ingredient6);
-
-          let measure6 = document.createElement('p');
-        measure6.innerHTML = drink.drinks[0][`strMeasure${6}`];
-        drinkSection.appendChild(measure6)
-
-       if(drink.drinks[0][`strMeasure${3}`] == null)
-            measure3.innerHTML = "";
-
-            if(drink.drinks[0][`strMeasure${4}`] == null)
-            measure4.innerHTML = "";
-
-            if(drink.drinks[0][`strMeasure${5}`] == null)
-            measure5.innerHTML = "";
-
-            if(drink.drinks[0][`strMeasure${6}`] == null)
-            measure6.innerHTML = "";
-       
-  if(drink.drinks[0][`strIngredient${3}`] == null)
-            ingredient3.innerHTML = "";
-
-            if(drink.drinks[0][`strIngredient${4}`] == null)
-            ingredient4.innerHTML = "";
-
-            if(drink.drinks[0][`strIngredient${5}`] == null)
-            ingredient5.innerHTML = "";
-            if(drink.drinks[0][`strIngredient${6}`] == null)
-            ingredient6.innerHTML = "";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+      
+    }
