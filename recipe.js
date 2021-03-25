58e12acd3ec0d305b57aef562e22188bb4f2ee1a
Beverly <beverlybuscher@gmail.com>
2021-03-24 21:31:35 -0500

    updated navbar, background image and responsiveness of grid

--- recipe.js
+++ recipe.js
@@ -8,6 +8,7 @@ const appID = '713a02bd';
 const apiKey = 'e8f480e6ca2fb549cf77a52d2356c058';
 const baseURL = `https://api.edamam.com/search?q=pizza`;
 
+
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     searchQuery = e.target.querySelector ("input").value;
@@ -33,14 +34,8 @@ function generateHTML(results){
                 <h4 class="title">${result.recipe.label}</h4>
                 <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
             </div>
-            <p class="item-data">Calories: ${result.recipe.calories.toFixed(0)}</p>
-            <p class="item-data">Diet label: ${
-                result.recipe.dietLabels.length > 0
-                  ? result.recipe.dietLabels
-                  : "No Data Found"
-              }</p>
-              <p class="item-data">Health-labels!!: ${result.recipe.healthLabels}</p>
-              <p class="item-data">Dish-Type: ${result.recipe.dishType}</p>
+              <p class="item-data">Calories: ${result.recipe.calories.toFixed(0)}</p>
+
               <p class="item-data">Ingredients: ${result.recipe.ingredientLines}</p>
             </div>
         </div>
@@ -114,7 +109,7 @@ function displayRandomCocktail(cocktail) {
 
     }
       
-    }
+}
 
     //code for swiper:
 
@@ -134,3 +129,4 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
       },
     });
+
